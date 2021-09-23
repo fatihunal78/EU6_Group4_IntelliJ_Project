b1aa6ee6bd38a5c77a770fbf94b4db40e2443f82
mehmetyetis <myetisbey2001@gmail.com>
2021-09-23 08:43:48 +0300

    yetisten de merhaba ok

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello!");
         System.out.println("Hello from Orhun!");
         System.out.println("Hello from yetis too");
+        System.out.println("ok");
     }
 }
